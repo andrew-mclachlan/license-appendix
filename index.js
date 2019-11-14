1b64758446b8783f250b39721ed7668c0ac561a1
SelvarajKaruppusamy <selvarajrch@gmail.com>
2019-11-14 17:09:28 +0530

    Generate different outputs from single truth
    Signed-off-by: SelvarajKaruppusamy <selvarajrch@gmail.com>

--- index.js
+++ index.js
@@ -42,40 +42,53 @@ checker.init({ start: program.path, production: true, customFormat: customFormat
     else {
         const outputFile = program.output;
         console.log("processing: " + program.path);
+
+        let licenses = Object.keys(packages).sort().map((key) => {
+            const package = packages[key];
+            const licenseFile = package.licenseFile ? path.basename(package.licenseFile) : "<<License File Not Found>>";
+            const licenseText = package.licenseText || "<<License Text Not Found>>";
+            const license = {
+                name: package.name,
+                version: package.version,
+                description: package.description,
+                licenses: package.licenses,
+                licenseFile: licenseFile,
+                licenseText: licenseText,
+                repository: package.repository || "<<License Repository Not Found>>",
+            };
+            return license;
+        });
+
         if (program.json) {
-            let licenses = Object.keys(packages).sort().map((key) => {
-                const package = packages[key];
-                const licenseFile = package.licenseFile ? path.basename(package.licenseFile) : "<<License File Not Found>>";
-                const licenseText = package.licenseText || "<<License Text Not Found>>";
-                const license = {
-                    "name": package.name,
-                    "version": package.version,
-                    "description": package.description,
-                    "licenses": package.licenses,
-                    "license_file": licenseFile,
-                    "license_text": licenseText.replace(/\n/g, "\\n").replace(/\r/g, "\\r").replace(/\t/g, "\\t"),
-                    "repository": package.repository || "<<License Repository Not Found>>",
-                };
-                return license;
+            licenses = licenses.map(licenseElem => {
+              let license = { ...licenseElem };
+
+              // camel case to hiphen case
+              license.license_file = license.licenseFile;
+              license.license_text = license.licenseText.replace(/\n/g, "\\n").replace(/\r/g, "\\r").replace(/\t/g, "\\t");
+              delete license.licenseFile;
+              delete license.licenseText;
+
+              return license;
             });
             fs.writeFileSync(outputFile, JSON.stringify(licenses, null, 2));
         }
         else {
-            fs.writeFileSync(outputFile, "####Appendix of packages and their licenses ####\n\n");
-            Object.keys(packages).sort().forEach((key) => {
-                const package = packages[key];
-                const licenseFile = package.licenseFile ? path.basename(package.licenseFile) : "<<License File Not Found>>";
-                let str = "#####" + "\n";
-                str += "name:\t" + package.name + "\n";
-                str += "version:\t" + package.version + "\n";
-                str += "description:\t" + package.description + "\n";
-                str += "repository:\t" + (package.repository || "<<License Repository Not Found>>") + "\n";
-                str += "license(s):\t" + package.licenses + "\n";
-                str += "license file:\t" + licenseFile + "\n";
-                str += "license text:\t" + (package.licenseText || "<<License Text Not Found>>") + "\n";
-                str += "#####" + "\n";
-                fs.appendFileSync(outputFile, str);
-            });
+            let str = "####Appendix of packages and their licenses ####\n\n";
+            str = licenses.reduce((acc, license) => {
+              let licenseStr = "#####" + "\n";
+              licenseStr += "name:\t" + license.name + "\n";
+              licenseStr += "version:\t" + license.version + "\n";
+              licenseStr += "description:\t" + license.description + "\n";
+              licenseStr += "repository:\t" + license.repository + "\n";
+              licenseStr += "license(s):\t" + license.licenses + "\n";
+              licenseStr += "license file:\t" + license.licenseFile + "\n";
+              licenseStr += "license text:\t" + license.licenseText + "\n";
+              licenseStr += "#####" + "\n";
+
+              return acc + licenseStr;
+            }, str);
+            fs.writeFileSync(outputFile, str);
         }
         console.log("Done.");
     }
