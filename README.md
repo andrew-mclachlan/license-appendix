96bab300471ed76a63b75a46f21a2f7f1cb7603e
SelvarajKaruppusamy <selvarajrch@gmail.com>
2019-11-15 17:31:53 +0530

    Fix broken licenses using  Github, readme search
    This change will perform the following actions in below order
     - for the broken licenses
    1. Search readme for license information with regex
    2. Search packages github repo for licenses (using your personal_access_token)
    
    Signed-off-by: SelvarajKaruppusamy <selvarajrch@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # license-appendix
 Create an appendix from the license-checker output.
+
+## Example commands
+**Attempt to fetch missing licenses from Github**
+
+`node index.js --json --path '/d/workspace/my_app/source' -o 'test.json' -a <your_github_personal_acces_token>
+`
+
+[Visit this link](https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line) to create your personal access token.
+
+
+**Fix only locally, not connect to Github**
+`node index.js --json --path '/d/workspace/my_app/source' -o 'test.json'`
--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const checker = require("license-checker");
 const fs = require("fs");
 const path = require("path");
 const program = require("commander");
+const fetch = require("node-fetch");
 
 program
     .version("0.1.1")
     .option("-j, --json", "output appendix in JSON format")
     .option("-p, --path [directory]", "package to scan")
     .option("-o, --output [file path]", "output appendix file path")
+    .option("-a, --auth [oauth token]", "github personal access token")
     .parse(process.argv);
 
 function isDirSync(checkPath) {
@@ -35,7 +37,142 @@ const customFormat = {
     "description": "<<Unknown Description>>"
 };
 
-checker.init({ start: program.path, production: true, customFormat: customFormat }, (error, packages) => {
+const getLicenseFileUrl = async (repoUrl, authToken) => {
+  let res = await fetch(repoUrl, {
+    headers: {
+      Authorization: 'Bearer ' + authToken
+    }
+  });
+  let licenseRegex = new RegExp(/^(license|licence)(.txt){0,}$/i);
+  if (res.status === 401) {
+    throw Error('Either token invalid or 5000 requests/hour quota expired');
+  }
+  if (res.status <= 400 && res.statusText === 'OK') {
+    let response = await res.text();
+    response = JSON.parse(response);
+    let licenseFile = response.find(elem => elem.type !== 'dir' && licenseRegex.test(elem.name));
+    return licenseFile && licenseFile.url;
+  }
+};
+
+const getLicenseFile = async (url, authToken) => {
+  let res = await fetch(url, {
+    headers: {
+      Authorization: 'Bearer ' + authToken
+    }
+  });
+  res = await res.text();
+  res = JSON.parse(res);
+  return Buffer.from(res.content, res.encoding).toString('ascii');
+};
+
+const fetchLicense = async (repoPath, authToken) => {
+  let repoUrl = 'https://api.github.com/repos/' + repoPath + '/contents/';
+  let licenseFileUrl = await getLicenseFileUrl(repoUrl, authToken);
+  if (licenseFileUrl) {
+    let license = await getLicenseFile(licenseFileUrl, authToken);
+    return license;
+  }
+};
+
+const fetchLicensesFromRepo = async (pkgs, authToken) =>  {
+  let validUrlRegex = new RegExp(/^http(s)?:\/\/github.com\/\w.*/);
+
+  let validPkgs = pkgs.filter(pkg => {
+    return validUrlRegex.test(pkg.repository);
+  }).map(pkg => {
+    return {
+      name: pkg.name,
+      repository: pkg.repository,
+      path: pkg.repository.match(new RegExp(/https:\/\/github.com\/(.*)/))[1]
+    };
+  });
+
+  try {
+    let results = [];
+    let responses = await Promise.all(validPkgs.map(pkg => fetchLicense(pkg.path, authToken)));
+
+    for (let i = 0; i < validPkgs.length; i++) {
+      let res = responses[i];
+      let pkg = validPkgs[i];
+      if (res) {
+        results.push({
+          name: pkg.name,
+          repository: pkg.repository,
+          licenseText: res
+        });
+      }
+    }
+    console.log('found github licenses for ', results.length, 'packages');
+    return results;
+  } catch(err) {
+      console.error(err);
+      console.error('Error fetching license file');
+  }
+};
+
+const updateGithubLicenses = (pkgs, githubLicenses) => {
+  for (let i = 0; i < pkgs.length; i++) {
+    const pkg = pkgs[i];
+    let matchingGithubLicense = githubLicenses.find(license => license.name === pkg.name);
+    if (matchingGithubLicense) {
+      pkg.licenseText = matchingGithubLicense.licenseText;
+      pkg.hasValidLicense = true;
+    }
+  }
+};
+
+const fixLicenseErrors = async (pkgsInfo, authToken) => {
+  // extract license from license text
+  let pkgs = [];
+  let copyrightRegex = new RegExp(/copyright/i);
+  for (let i = 0; i < pkgsInfo.length; i++) {
+    const pkg = {
+      ...pkgsInfo[i]
+    };
+    let matches = pkg.licenseText.match(new RegExp(/(.|\\n){0,}#{1,}\s{0,}license\s{1,}(.[^#]*)/i));
+    let licenseText = (matches && matches[2]) || '';
+    licenseText = licenseText.trim();
+    if (licenseText.length) {
+      if (copyrightRegex.test(licenseText) && licenseText.split('\n').length >= 2) {
+        pkg.licenseText = licenseText;
+        pkg.hasValidLicense = true;
+      } else {
+        // TODO: to be used
+        pkg.extractedLicense = licenseText;
+      }
+    }
+    pkgs.push(pkg);
+  }
+
+  // search in Github for license files - some packages might have added license file
+  if (authToken) {
+    let githubLicenses = await fetchLicensesFromRepo(
+      pkgs.filter(pkg => !pkg.hasValidLicense), authToken
+    ) || [];
+    updateGithubLicenses(pkgs, githubLicenses);
+  }
+
+  //TODO: fill incomplete licenses
+
+  pkgs = pkgs.filter(pkg => pkg.hasValidLicense);
+  return pkgs;
+};
+
+const updateFixedLicenses = (licenses, fixedLicenses) => {
+  for (let i = 0; i < fixedLicenses.length; i++) {
+    const fixedLicense = fixedLicenses[i];
+    const licenseIndex = licenses.findIndex(license => license.name === fixedLicense.name);
+    let license = licenses[licenseIndex];
+    license.licenseText = fixedLicense.licenseText
+    // Remove license-checker guesses
+    if (typeof license.licenses === 'string' && license.licenses.endsWith('*')) {
+      license.licenses = license.licenses.substr(0, license.licenses.length-1);
+    }
+  }
+};
+
+checker.init({ start: program.path, production: true, customFormat: customFormat }, async (error, packages) => {
     if (error) {
         throw new Error(error);
     }
@@ -58,6 +195,17 @@ checker.init({ start: program.path, production: true, customFormat: customFormat
             };
             return license;
         });
+
+        /* attempt to fix broken licenses
+        * Most likely packages that contain README(.MD) as the license-file will have erroneous license-text
+        */
+        let licenseErrors = licenses.filter(license => {
+          let licenseFile = license.licenseFile.toLowerCase();
+          return licenseFile.indexOf('not found') !== -1 || licenseFile.indexOf('readme') !== -1;
+        });
+
+        let fixedLicenses = await fixLicenseErrors(licenseErrors, program.auth);
+        updateFixedLicenses(licenses, fixedLicenses);
 
         if (program.json) {
             licenses = licenses.map(licenseElem => {
